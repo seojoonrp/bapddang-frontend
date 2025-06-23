f182de9e572643003043f30bb3eab20e6a95d747
seojoonrp <seojoonrp@snu.ac.kr>
2025-06-23 15:23:28 +0900

    added basic data frame

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,9 +1,25 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text, Switch } from "react-native";
 
 const MainScreen = () => {
+  const [isFast, setIsFast] = useState(true);
+
+  const toggleFast = () => {
+    setIsFast(prev => !prev);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>텍스트임</Text>
+      <View style={styles.switchWrapper}>
+        <Switch
+          style={styles.switch}
+          trackColor={{ false: '#359c21', true: '#e02828' }}
+          ios_backgroundColor='#359c21'
+          thumbColor='#fcfcfc'
+          onValueChange={toggleFast}
+          value={isFast}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +30,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: 'white',
   },
+  switchWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 20,
+
+    borderColor: 'black',
+  },
+  switch: {
+    marginRight: 20,
+  }
 })
