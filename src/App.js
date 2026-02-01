3f4e7df357852eef7cf60b7c076b43327d9ec47d
seojoonrp <seojoonrp@snu.ac.kr>
2026-02-01 18:38:27 +0900

    feat: Change font to 11000 ttf

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ const App = () => {
 
   useEffect(() => {
     Font.loadAsync({
-      NanumSquareEB: require("./assets/fonts/NanumSquareEB.woff2"),
-      NanumSquareB: require("./assets/fonts/NanumSquareB.woff2"),
-      NanumSquareR: require("./assets/fonts/NanumSquareR.woff2"),
-      NanumSquareL: require("./assets/fonts/NanumSquareL.woff2"),
-      NanumSquareRoundEB: require("./assets/fonts/NanumSquareRoundEB.woff2"),
-      NanumSquareRoundB: require("./assets/fonts/NanumSquareRoundB.woff2"),
-      NanumSquareRoundR: require("./assets/fonts/NanumSquareRoundR.woff2"),
-      NanumSquareRoundL: require("./assets/fonts/NanumSquareRoundL.woff2"),
-      KCCGanpan: require("./assets/fonts/KCC-Ganpan.woff2"),
+      NanumSquareEB: require("./assets/fonts/NanumSquareOTF_acEB.otf"),
+      NanumSquareB: require("./assets/fonts/NanumSquareOTF_acB.otf"),
+      NanumSquareR: require("./assets/fonts/NanumSquareOTF_acR.otf"),
+      NanumSquareL: require("./assets/fonts/NanumSquareOTF_acL.otf"),
+      NanumSquareRoundEB: require("./assets/fonts/NanumSquareRoundOTFEB.otf"),
+      NanumSquareRoundB: require("./assets/fonts/NanumSquareRoundOTFB.otf"),
+      NanumSquareRoundR: require("./assets/fonts/NanumSquareRoundOTFR.otf"),
+      NanumSquareRoundL: require("./assets/fonts/NanumSquareRoundOTFL.otf"),
+      KCCGanpan: require("./assets/fonts/KCC-Ganpan.ttf"),
     }).then(() => setFontsLoaded(true));
 
     initGoogleLogin();
